33ccf294cb8c75e23a79b1d39b137971a3364b62
Manuel Carbajal <manuel@montebit.io>
2024-01-10 13:40:27 -0600

    feat: add error returns and refactor duplicated code

--- pagination.go
+++ pagination.go
@@ -9,10 +9,10 @@ import (
 // SelectPages get data using requested query
 func (c *Page[T]) SelectPages(pager *GormPager, query *gorm.DB) error {
 	var model T
-	query.Model(&model).Count(&c.TotalEntries)
-	c.checkPageSizeLimits(pager.options.PageSizeLowerLimit, pager.options.PageSizeUpperLimit)
-	c.setTotalPages()
-	if err := c.checkPageCanBeCreated(); err != nil {
+	if res := query.Model(&model).Count(&c.TotalEntries); res.Error != nil {
+		return res.Error
+	}
+	if err := c.setPageSizeAndTotalPages(pager.options.PageSizeLowerLimit, pager.options.PageSizeUpperLimit); err != nil {
 		return err
 	}
 	if res := query.Model(&model).Scopes(func(db *gorm.DB) *gorm.DB {
@@ -21,25 +21,28 @@ func (c *Page[T]) SelectPages(pager *GormPager, query *gorm.DB) error {
 	}).Find(&c.Data); res.Error != nil {
 		return res.Error
 	}
-	c.calculateNextPage()
-	c.setEntriesCount()
+
+	c.setNextPageAndEntriesCount()
+
 	return nil
 }
 
 // SelectPagesRow is used when you have a raw sql and you want to paginate that result
 func (c *Page[T]) SelectPagesRow(pager *GormPager, countRawQuery, rawQuery *gorm.DB) error {
-	countRawQuery.Scan(&c.TotalEntries)
-	c.checkPageSizeLimits(pager.options.PageSizeLowerLimit, pager.options.PageSizeUpperLimit)
-	c.setTotalPages()
-	if err := c.checkPageCanBeCreated(); err != nil {
+	if res := countRawQuery.Scan(&c.TotalEntries); res.Error != nil {
+		return res.Error
+	}
+
+	if err := c.setPageSizeAndTotalPages(pager.options.PageSizeLowerLimit, pager.options.PageSizeUpperLimit); err != nil {
 		return err
 	}
 
 	if res := rawQuery.Offset(c.getOffset()).Limit(int(c.PageSize)).Scan(&c.Data); res.Error != nil {
 		return res.Error
 	}
-	c.calculateNextPage()
-	c.setEntriesCount()
+
+	c.setNextPageAndEntriesCount()
+
 	return nil
 }
 
@@ -48,27 +51,33 @@ func (c *Page[T]) HasNextPage() bool {
 	return c.CurrentPage < c.TotalPages
 }
 
-func (c *Page[T]) setTotalPages() {
-	var (
-		totalEntriesF = float64(c.TotalEntries)
-		pageSizeF     = float64(c.PageSize)
-		totalPagesF   = math.Ceil(totalEntriesF / pageSizeF)
-	)
-	c.TotalPages = int64(totalPagesF)
-	if c.TotalPages == 0 {
-		c.TotalPages = 1
+func (c *Page[T]) setPageSizeAndTotalPages(lower, upper uint) error {
+	setTotalPages := func() {
+		var (
+			totalEntriesF = float64(c.TotalEntries)
+			pageSizeF     = float64(c.PageSize)
+			totalPagesF   = math.Ceil(totalEntriesF / pageSizeF)
+		)
+		c.TotalPages = int64(totalPagesF)
+		if c.TotalPages == 0 {
+			c.TotalPages = 1
+		}
 	}
-}
 
-func (c *Page[T]) checkPageSizeLimits(lower, upper uint) {
-	if c.PageSize > int64(upper) {
-		c.PageSize = int64(upper)
-		return
+	checkPageSizeLimits := func(lower, upper uint) {
+		if c.PageSize > int64(upper) {
+			c.PageSize = int64(upper)
+			return
+		}
+		if c.PageSize < int64(lower) {
+			c.PageSize = int64(lower)
+			return
+		}
 	}
-	if c.PageSize < int64(lower) {
-		c.PageSize = int64(lower)
-		return
-	}
+
+	checkPageSizeLimits(lower, upper)
+	setTotalPages()
+	return c.checkPageCanBeCreated()
 }
 
 func (c *Page[T]) checkPageCanBeCreated() error {
@@ -78,20 +87,24 @@ func (c *Page[T]) checkPageCanBeCreated() error {
 	return nil
 }
 
-func (c *Page[T]) calculateNextPage() {
-	if len(c.Data) == 0 {
-		c.NextPage = 1
-		return
+func (c *Page[T]) setNextPageAndEntriesCount() {
+	calculateNextPage := func() {
+		if len(c.Data) == 0 {
+			c.NextPage = 1
+			return
+		}
+		if c.CurrentPage == c.TotalPages {
+			c.NextPage = 1
+			return
+		}
+		c.NextPage = c.CurrentPage + 1
 	}
-	if c.CurrentPage == c.TotalPages {
-		c.NextPage = 1
-		return
+	setEntriesCount := func() {
+		c.EntriesCount = int64(len(c.Data))
 	}
-	c.NextPage = c.CurrentPage + 1
-}
 
-func (c *Page[T]) setEntriesCount() {
-	c.EntriesCount = int64(len(c.Data))
+	calculateNextPage()
+	setEntriesCount()
 }
 
 func (c *Page[T]) getOffset() int {
